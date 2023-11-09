e63ff12572cfb1a7a0fd6d4a4cad8a32e9fe5fdc
LocTM <tranminhloc.anhk@gmail.com>
2023-11-09 08:38:19 +0700

    fisrt

---
+++ index.js
@@ -0,0 +1,31 @@
+let i =10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+
+document.write("i = " +i)
+document.write("<br/>")
+document.write("f = " +f)
+document.write("<br/>")
+document.write("b = " +b)
+document.write("<br/>")
+document.write("s = " +s)
+document.write("<br/>")
+
+
+
+let width = 20
+let height = 10
+let area = width * height
+document.write("Area = " + area)
+
+
+
+a = prompt("Nhập số a: ")
+c = prompt("Nhập số c: ")
+
+    if (a % c === 0) {
+        alert("a chia hết cho c")
+    }
+    
